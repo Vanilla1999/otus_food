4d925f26c124784d2827edf91ce8441015b37587
Vanilla <vetal-vetal-vv@mail.ru>
2023-03-26 13:22:08 +0300

    refactor images

--- lib/data/repository/recipe_repo.dart
+++ lib/data/repository/recipe_repo.dart
@@ -4,11 +4,11 @@ class MockRecipeRepo extends RecipeRepo{
   @override
   List<Recipe> getRecipes() {
     return [
-      Recipe(1, "Лосось в соусе терияки", "45 минут", "asset/lococ.jpg"),
+      Recipe(1, "Лосось в соусе терияки", "45 минут", "asset/one.jpg"),
       Recipe(2, "Поке боул с сыром тофу", "30 минут", "asset/two.jpg"),
       Recipe(3, "Стейк из говядины по-грузински с кукурузой", "1 час 15 минут", "asset/three.jpg"),
       Recipe(4, "Тосты с голубикой и бананом", "45 минут", "asset/four.jpg"),
-      Recipe(5, "Паста с морепродуктами", "25 минут", "asset/piza.jpg"),
+      Recipe(5, "Паста с морепродуктами", "25 минут", "asset/seven.jpg"),
       Recipe(6, "Бургер с двумя котлетами", "1 час", "asset/five.jpg"),
       Recipe(7, "Пицца Маргарита домашняя", "25 минут", "asset/six.jpg"),
     ];
