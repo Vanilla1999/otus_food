6270acf7ef4e00f5cc887cdf9ad79643e499fc04
Vanilla <vetal-vetal-vv@mail.ru>
2023-05-03 19:10:18 +0300

    add animation on Transition

--- lib/navigation/main_navigation.dart
+++ lib/navigation/main_navigation.dart
@@ -20,18 +20,53 @@ class MainNavigation {
           builder: (_) => _screenFactory.home(),
         );
       case MainNavigationRouteNames.recipeListScreen:
-        return MaterialPageRoute(
-          builder: (_) => _screenFactory.makeRecipeListScreen(),
-        );
+        return _createRoute(_screenFactory.makeRecipeListScreen());
       case MainNavigationRouteNames.recipeDescriptionScreen:
         final arguments = settings.arguments;
         final recipe = arguments as Recipe;
-        return MaterialPageRoute(
-          builder: (_) => _screenFactory.makeRecipeDescriptionScreen(recipe),
-        );
+        return _createRoute(_screenFactory.makeRecipeDescriptionScreen(recipe));
       default:
         const widget = Text('Navigation error!!!');
         return MaterialPageRoute(builder: (_) => widget);
     }
+  }
+
+  Route<Object> _createRoute(Widget widget) {
+    return PageRouteBuilder(
+      transitionDuration: Duration(milliseconds: 1500),
+      pageBuilder: (context, animation, secondaryAnimation) => widget,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin1 = Offset(1.0, 0.0);
+        const end1 = Offset(-0.03, 0.0);
+        const begin2 = Offset(-0.03, 0.0);
+        const end2 = Offset(0.0, 0.0);
+        final tween = Tween(begin: 0.0, end: 1.0);
+        final scaleAnimation = TweenSequence(
+          <TweenSequenceItem<Offset>>[
+            TweenSequenceItem<Offset>(
+              tween: Tween(begin: begin1, end: end1)
+                  .chain(CurveTween(curve: Curves.ease)),
+              weight: 50.0,
+            ),
+            TweenSequenceItem<Offset>(
+              tween: Tween(begin: begin2, end: end2)
+                  .chain(CurveTween(curve: Curves.ease)),
+              weight: 50.0,
+            ),
+          ],
+        );
+        if (animation.status == AnimationStatus.reverse) {
+          return FadeTransition(
+            opacity: animation.drive(tween),
+            child: child,
+          );
+        } else {
+          return SlideTransition(
+            position: animation.drive(scaleAnimation),
+            child: child,
+          );
+        }
+      },
+    );
   }
 }
